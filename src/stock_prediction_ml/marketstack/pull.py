81eb1d0291e51249c79a4078cd07eb927e7dc995
Raydi <duong.tranhn1102@gmail.com>
2025-10-29 18:22:49 +0100

    feat: add script to pull API data

---
+++ src/stock_prediction_ml/marketstack/pull.py
@@ -0,0 +1,29 @@
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def load_config():
+    pass
+
+
+def fetch_eod_with_date():
+    pass
+
+
+def process_dataframe():
+    pass
+
+
+def save_to_parquet():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
