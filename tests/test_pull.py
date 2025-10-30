88c24dc6d511630df5dcabcc193fe3ca4ecd00c8
Raydi <duong.tranhn1102@gmail.com>
2025-10-30 15:46:39 +0100

    tests: correct test cases

--- tests/test_pull.py
+++ tests/test_pull.py
@@ -14,17 +14,15 @@ def test_load_config_output_api_key():
     assert isinstance(api_key, str)
 
 
-def test_fetch_eod_with_date_return_dict():
+def test_fetch_eod_with_date_return_list():
     api_key = load_config()
     data = fetch_eod_with_date(api_key, "AAPL", "2025-01-01", "2025-01-02")
-    assert isinstance(data, dict)
-    assert "data" in data
-    assert isinstance(data["data"], list)
+    assert isinstance(data, list)
 
 
 @pytest.mark.parametrize(
     "column",
-    ["date", "close", "volume", "adjusted_close", "symbol", "open", "high", "low"],
+    ["date", "close", "volume", "adj_close", "symbol", "open", "high", "low"],
 )
 def test_process_dataframe_output_dataframe_with_correct_column(column):
     api_key = load_config()
@@ -36,10 +34,10 @@ def test_process_dataframe_output_dataframe_with_correct_column(column):
 
 def test_save_to_parquet_creates_file(tmp_path):
     api_key = load_config()
-    data = fetch_eod_with_date(api_key, "AAPL", "2025-01-01", "2025-01-02")
+    data = fetch_eod_with_date(api_key, ["AAPL"], "2025-01-01", "2025-01-02")
     df = process_dataframe(data)
     file_path = tmp_path / "test.parquet"
-    save_to_parquet(df, file_path)
-    assert file_path.exists(), "Parquet file was not created."
+    save_to_parquet(df, str(file_path))
+    assert file_path.exists()
     loaded_df = pd.read_parquet(file_path)
     pd.testing.assert_frame_equal(df, loaded_df)
