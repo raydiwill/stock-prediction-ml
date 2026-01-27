cd1caa56e8da0e1a945398441df36400753daee6
Raydi <duong.tranhn1102@gmail.com>
2026-01-27 15:49:50 +0100

    feat: add prediction service

---
+++ src/stock_prediction_ml/feast/feature_services.py
@@ -0,0 +1,19 @@
+from feast import FeatureService
+
+
+from .features_definition import (
+    stock_basic_features,
+    stock_technical_features,
+    stock_timeseries_features,
+)
+
+
+stock_prediction_service = FeatureService(
+    name="stock_prediction_service",
+    features=[
+        stock_basic_features,
+        stock_technical_features,
+        stock_timeseries_features,
+    ],
+    tags={"model": "catboost_v1"},
+)
