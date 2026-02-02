f58c2e5a159df9d08a773d3b6b3a80f1a0527fb5
Raydi <duong.tranhn1102@gmail.com>
2026-02-02 12:06:50 +0100

    feat: add api setting

--- src/stock_prediction_ml/config/settings.py
+++ src/stock_prediction_ml/config/settings.py
@@ -1,17 +1,43 @@
+import os
+from pathlib import Path
+from typing import List
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
 )
 
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+DEFAULT_ENV_FILE = PROJECT_ROOT / "configs" / "config.env.local"
+
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite:///./dev.db"  # default for local dev
+    # Default settings for local def
+
+    database_url: str = "sqlite:///./dev.db"
+
     mlflow_tracking_uri: str = "file:./mlruns"
-    model_name: str = "catboost_model"
 
     marketstack_api_key: str | None = None
 
-    model_config = SettingsConfigDict(env_file="config.env")
+    api_host: str = "127.0.0.1"
+    api_port: int = 8000
+    api_log_level: str = "debug"
+    api_reload: bool = True
+
+    # Serving model
+    model_name: str = "catboost_model"
+    default_experiment_name: str = "stock_prediction_experiment"
+
+    cors_origins: str = "*"
+
+    @property
+    def split_cors_origins_to_list(self) -> List[str]:
+        """Conver cors_origin string to list for FastAPI"""
+        return [origin.strip() for origin in self.cors_origins.split(",")]
+
+    model_config = SettingsConfigDict(
+        env_file=os.getenv("ENV_FILE", str(DEFAULT_ENV_FILE)), env_file_encoding="utf_8"
+    )
 
 
 settings = Settings()
